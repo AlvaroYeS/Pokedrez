e73a2839ecc3350312e1c183f9f386ac8efad414
AlvaroYS <1808.ays@gmail.com>
2024-11-25 19:44:05 +0000

    Primer FrontEnd commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Proyecto APB- Fase inicial
+
+## 1º Informe sobre el proyecto:
+
+En este proyecto, desarrollaremos un videojuego estratégico que fusiona elementos icónicos de Pokémon y el estilo táctico de Teamfight Tactics (TFT). El objetivo es ofrecer una experiencia única que combine el estilo pokémon con la idea de desplegarlos en un tablero para que se adapten a cada partida para nosotros representa un reto en el que queremos abarcar los conocimientos que tenemos de las materias que estamos cursando, además de experimentar con un trabajo en equipo tan elaborado.
+
+**Meta del proyecto:** Crear un juego que sea funcional, desafiante y atractivo tanto para los fans de Pokémon como para los entusiastas de los juegos de estrategia como TFT. Buscamos destacar por una jugabilidad innovadora y una experiencia que fomente la rejugabilidad.
+
+**Equipo**: Este proyecto será llevado a cabo por mi(Saúl), Victor y Álvaro
+
+<3
---
+++ src/main.kt
@@ -0,0 +1,38 @@
+import korlibs.time.*
+import korlibs.korge.*
+import korlibs.korge.scene.*
+import korlibs.korge.tween.*
+import korlibs.korge.view.*
+import korlibs.image.color.*
+import korlibs.image.format.*
+import korlibs.io.file.*
+import korlibs.io.file.std.*
+import korlibs.math.*
+import korlibs.math.geom.*
+import korlibs.math.interpolation.*
+import kotlin.random.*
+
+suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
+	val sceneContainer = sceneContainer()
+
+	sceneContainer.changeTo { MyScene() }
+}
+
+class MyScene : PixelatedScene(128 * 10, 128 * 10, sceneSmoothing = true) {
+	override suspend fun SContainer.sceneMain() {
+        val bgs = listOf(resourcesVfs["bgs/square brown dark.png"].readBitmap(), resourcesVfs["bgs/square brown light.png"].readBitmap())
+
+        for (x in 0 until 8) {
+            for (y in 0 until 8) {
+                image(bgs[(x + y).isOdd.toInt()]).xy(128 * x, 128 * y)
+            }
+        }
+
+        for (n in 0 until 8) {
+            val num = Random.nextInt(1, 151)
+            val string = "pokemon/icons/$num.png"
+            val image = resourcesVfs[string].readBitmap()
+            image(image).xy(128 * n + 64, 0 + 64).scale(3).anchor(0.50, 0.50)
+        }
+	}
+}
