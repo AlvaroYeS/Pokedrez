66d641e11bfb91be0c0dbb688017809f5a3469fe
AlvaroYS <1808.ays@gmail.com>
2024-11-26 18:50:02 +0000

    Panel tienda y objetos añadido

--- src/main.kt
+++ src/main.kt
@@ -18,22 +18,52 @@ suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors
 	sceneContainer.changeTo { MyScene() }
 }
 
-class MyScene : PixelatedScene(128 * 10, 128 * 10, sceneSmoothing = true) {
+class MyScene : PixelatedScene(128 * 12, 128 * 12, sceneSmoothing = true) {
 	override suspend fun SContainer.sceneMain() {
         image(resourcesVfs["bgs/BattleBgForest.jpg"].readBitmap()).xy(0, 0).scale(3)
         val bgs = listOf(resourcesVfs["bgs/square brown dark.png"].readBitmap(), resourcesVfs["bgs/square brown light.png"].readBitmap())
 
+        //Tablero
         for (x in 0 until 8) {
             for (y in 0 until 8) {
                 image(bgs[(x + y).isOdd.toInt()]).xy(128 * x + 128, 128 * y + 128)
             }
         }
 
+        //Primera Fila
         for (n in 0 until 8) {
             val num = Random.nextInt(1, 151)
             val string = "pokemon/icons/$num.png"
             val image = resourcesVfs[string].readBitmap()
             image(image).xy(128 * n + 192, 0 + 192).scale(3).anchor(0.50, 0.50)
+        }
+
+        //Fondo Tienda
+        for (x in 0 until 8) {
+            val image = resourcesVfs["bgs/square gray light.png"].readBitmap()
+            image(image).xy(128 * x + 192, (9 * 128) + 192).anchor(0.50, 0.50)
+        }
+
+        //Tienda
+        for (n in 0 until 8) {
+            val num = Random.nextInt(1, 151)
+            val string = "pokemon/icons/$num.png"
+            val image = resourcesVfs[string].readBitmap()
+            image(image).xy(128 * n + 192, (9 * 128) + 192).scale(3).anchor(0.50, 0.50)
+        }
+
+        //Fondo Objetos
+        for (x in 0 until 8) {
+            val image = resourcesVfs["bgs/square gray dark.png"].readBitmap()
+            image(image).xy((9 * 128) + 192, 128 * x + 192).anchor(0.50, 0.50)
+        }
+
+        //Objetos
+        for (n in 0 until 8) {
+            val num = Random.nextInt(1, 151)
+            val string = "pokemon/icons/$num.png"
+            val image = resourcesVfs[string].readBitmap()
+            image(image).xy((9 * 128) + 192, 128 * n + 192).scale(3).anchor(0.50, 0.50)
         }
 	}
 }
