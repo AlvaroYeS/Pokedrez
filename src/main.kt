f4ac69fd2728f2dfc13a4ddd833adc5edbb8b9e8
Alvaroyelamos <1808.ays@gmail.com>
2024-11-26 00:37:57 +0000

    bg change

--- src/main.kt
+++ src/main.kt
@@ -20,12 +20,12 @@ suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors
 
 class MyScene : PixelatedScene(128 * 10, 128 * 10, sceneSmoothing = true) {
 	override suspend fun SContainer.sceneMain() {
-        image(resourcesVfs["bgs/field.jpg"].readBitmap()).xy(0, 0)
+        image(resourcesVfs["bgs/BattleBgForest.jpg"].readBitmap()).xy(0, 0).scale(3)
         val bgs = listOf(resourcesVfs["bgs/square brown dark.png"].readBitmap(), resourcesVfs["bgs/square brown light.png"].readBitmap())
 
         for (x in 0 until 8) {
             for (y in 0 until 8) {
-                image(bgs[(x + y).isOdd.toInt()]).xy(128 * x, 128 * y)
+                image(bgs[(x + y).isOdd.toInt()]).xy(128 * x + 128, 128 * y + 128)
             }
         }
 
@@ -33,7 +33,7 @@ class MyScene : PixelatedScene(128 * 10, 128 * 10, sceneSmoothing = true) {
             val num = Random.nextInt(1, 151)
             val string = "pokemon/icons/$num.png"
             val image = resourcesVfs[string].readBitmap()
-            image(image).xy(128 * n + 64, 0 + 64).scale(3).anchor(0.50, 0.50)
+            image(image).xy(128 * n + 192, 0 + 192).scale(3).anchor(0.50, 0.50)
         }
 	}
 }
